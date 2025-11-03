8a19b4e3e75fc4c6006cb5b05b224fa8a2678ca1
GoldenHuntingBear <beringer.lam@gmail.com>
2025-11-03 19:32:23 +0100

    Working better before adding transition

--- scenes/active_wave.gd
+++ scenes/active_wave.gd
@@ -1,10 +1,11 @@
 extends Path2D
 
 @onready var ui_controller: UIController = $"../Control"
+@export var speed:float = 10
 
 func _process(delta: float) -> void:
-	position.y -= delta
-	curve.add_point(Vector2(Time.get_ticks_msec()/1000, ui_controller.my_function(Time.get_ticks_msec()/1000)))
+	position.x -= speed*delta
+	curve.add_point(Vector2(speed*Time.get_ticks_msec()/1000, ui_controller.my_function(speed*Time.get_ticks_msec()/1000)))
 	if curve.point_count >= 500:
 		curve.remove_point(0)
 	queue_redraw()
@@ -13,4 +14,4 @@ func _process(delta: float) -> void:
 func _draw():
 	var points = curve.get_baked_points()
 	if points:
-		draw_polyline(points, Color.BLUE, 4, true)
+		draw_polyline(points, Color.BLUE, 2, true)
